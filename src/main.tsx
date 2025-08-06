c071751147a716296121ffafc1716ddd91498e22
Hasan Mehedi <drzraju@gmail.com>
2025-08-06 06:06:39 +0600

    first main

--- src/main.tsx
+++ src/main.tsx
@@ -2,19 +2,19 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { HashRouter , Routes, Route } from 'react-router';
+import { BrowserRouter, Routes, Route } from 'react-router';
 import AirdropPage from './components/AirdropPage.jsx'
 import Auth from './components/Auth.jsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <HashRouter >
+    <BrowserRouter>
     <Routes>
       <Route path='/' element={<App/>}/>
       <Route path='/airdrop' element={<AirdropPage/>}/>
       <Route path='/airdrop/auth' element={<Auth/>}/>
 
     </Routes>
-    </HashRouter >
+    </BrowserRouter>
   </StrictMode>
 );
